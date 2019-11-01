402bdb844f86673103cff4ad3c0c08701a1e4793
Elizabeth Yu <elizabethyu@pursuit.org>
2019-11-01 12:41:34 -0400

    dogCall

--- app/src/main/java/org/pursuit/dogapiagain/model/Dog.java
+++ app/src/main/java/org/pursuit/dogapiagain/model/Dog.java
@@ -1,6 +1,18 @@
 package org.pursuit.dogapiagain.model;
 
+import java.util.List;
+
 public class Dog {
+    List<String> message;
+
+    public Dog(List<String> message) {
+        this.message = message;
+    }
+
+    public List<String> getMessage() {
+        return message;
+    }
+
     String dogName;
 
     public Dog(String dogName) {
