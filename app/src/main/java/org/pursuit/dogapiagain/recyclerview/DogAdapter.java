4d9acba86c21ad4d536cc74de28bf751bdb971bd
khaisoe <khaingsoe@pursuit.org>
2019-11-06 21:15:52 -0500

    added classes and layout xmls for recyclerview and fragments

---
+++ app/src/main/java/org/pursuit/dogapiagain/recyclerview/DogAdapter.java
@@ -0,0 +1,24 @@
+package org.pursuit.dogapiagain.recyclerview;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DogAdapter extends RecyclerView.Adapter<DogViewHolder> {
+    @NonNull
+    @Override
+    public DogViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DogViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/org/pursuit/dogapiagain/recyclerview/DogViewHolder.java
@@ -0,0 +1,12 @@
+package org.pursuit.dogapiagain.recyclerview;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DogViewHolder extends RecyclerView.ViewHolder {
+    public DogViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
