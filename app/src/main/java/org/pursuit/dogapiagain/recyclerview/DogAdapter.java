38c31d9e61f165d316b1be9d0cb0539d0dac7c57
khaisoe <khaingsoe@pursuit.org>
2019-11-08 13:09:03 -0500

    continuation of recyclerView

--- app/src/main/java/org/pursuit/dogapiagain/recyclerview/DogAdapter.java
+++ app/src/main/java/org/pursuit/dogapiagain/recyclerview/DogAdapter.java
@@ -1,11 +1,22 @@
 package org.pursuit.dogapiagain.recyclerview;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.pursuit.dogapiagain.fragments.FragmentInterface;
+import org.pursuit.dogapiagain.model.Dog;
+
+import java.util.List;
+
 public class DogAdapter extends RecyclerView.Adapter<DogViewHolder> {
+
+    private List<Dog> dogList;
+    private FragmentInterface fragmentInterface;
+
     @NonNull
     @Override
     public DogViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
--- app/src/main/java/org/pursuit/dogapiagain/recyclerview/DogViewHolder.java
+++ app/src/main/java/org/pursuit/dogapiagain/recyclerview/DogViewHolder.java
@@ -5,7 +5,13 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.pursuit.dogapiagain.fragments.FragmentInterface;
+
 public class DogViewHolder extends RecyclerView.ViewHolder {
+
+    private FragmentInterface fragmentInterface;
+
+
     public DogViewHolder(@NonNull View itemView) {
         super(itemView);
     }
